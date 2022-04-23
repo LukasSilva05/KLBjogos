abf382a4f3fbae5fe17b58421cd41fd51e3592e1
Lukas Silva <lgs20@aluno.ifal.edu.br>
2022-04-23 16:38:51 -0300

    tentativas e MUITOS erros

--- carrinho.js
+++ carrinho.js
@@ -1,4 +1,4 @@
-﻿import React, { useState } from 'react';
+﻿import React, { useState, useEffect } from 'react';
 import {
     View,
     Text,
@@ -10,89 +10,23 @@ import {
 import { Entypo, AntDesign, FontAwesome5 } from '@expo/vector-icons';
 import styles from './style';
 import { ScrollView } from 'react-native-gesture-handler';
+import axios from 'axios';
+import api from './api';
 
-function Carrinho({ route, navigation }) {
+function Carrinho({ navigation }) {
 
-    const [lancamentos, setimages] = useState([
-        {
-            id: 1,
-            name: "Elden Ring",
-            value: "R$ 249,90",
-            image: require("./assets/eldenring.jpg"),
-            data: "25 de fevereiro de 2022",
-            desenvolvedor: "FromSoftware",
-            plataforma:
-                "PlayStation 4, Xbox Series X, Xbox One, PlayStation 5, Microsoft Windows",
-            genero: "Aventura, RPG, Ação",
-            idade: require("./assets/classificacao-16-anos.png"),
-            sobre: 'O NOVO RPG DE AÇÃO E FANTASIA. Levante-se, Maculado, e seja guiado pela graça para portar o poder do Anel Prístino e se tornar um Lorde Prístino nas Terras Intermédias.',
-            requisitos: {
-                so: 'Windows 10/11',
-                processador: 'INTEL CORE I7-8700K or AMD RYZEN 5 3600X',
-                memoria: '16 GB de RAM',
-                placa: 'NVIDIA GEFORCE GTX 1070 8 GB or AMD RADEON RX VEGA 56 8 GB',
-                armazenamento: '60 GB'
-            }
-        },
-        {
-            id: 2,
-            name: "Call Of Duty: Modern Warfare",
-            value: "R$ 65,96",
-            image: require("./assets/cod.jpg"),
-            data: "25 de outubro de 2019",
-            desenvolvedor: "Infinity Ward",
-            plataforma: "PlayStation 4, Xbox One, Microsoft Windows",
-            genero: "Ação",
-            idade: require("./assets/classificacao-16-anos.png"),
-            sobre: 'Um dos jogos mais aclamados pela crítica na história, Call of Duty®4: Modern Warfare® está de volta, remasterizado em alta definição com melhores texturas, renderização mais realista, efeitos de luz dinâmicos e muito mais para uma nova geração.',
-            requisitos: {
-                so: 'Windows 7 64-Bit or later',
-                processador: 'Intel Core i3-3225 @ 3.30GHz or equivalent',
-                memoria: '8 GB de RAM',
-                placa: 'NVIDIA GeForce GTX 660 2GB / AMD Radeon HD 7850 2GB',
-                armazenamento: '20 GB'
-            }
-        },
-        {
-            id: 3,
-            name: "Bus Simulator 21",
-            value: "R$ 117,99",
-            image: require("./assets/bus21.jpg"),
-            data: "07 de setembro de 2021",
-            desenvolvedor: "Stillalive Studios",
-            plataforma:
-                "Xbox Series X, Xbox One, PlayStation 4, Microsoft Windows, PlayStation 5",
-            genero: "Simulação",
-            idade: require("./assets/classificacao-14-anos.png"),
-            sobre: 'Experimente o dia a dia de um motorista de ônibus em duas cidades abertas nos EUA ou Europa. Dirija até 30 ônibus licenciados de fabricantes internacionais, incluindo ônibus de dois andares e elétricos.',
-            requisitos: {
-                so: 'Windows 10 64-Bit',
-                processador: 'Intel Core i5-4440 / AMD FC-8140 ou equivalente',
-                memoria: '8 GB de RAM',
-                placa: 'NVIDIA GeForce GTX 1070 (8 GB de VRAM) / AMD Radeon RX Vega 56 (8 GB de VRAM) ou superior',
-                armazenamento: '18 GB'
-            }
-        },
-        {
-            id: 4,
-            name: "Microsoft Flight Simulator",
-            value: "R$ 249,95",
-            image: require("./assets/pesado.jpg"),
-            data: "18 de agosto de 2020",
-            desenvolvedor: "Asobo Studio",
-            plataforma: "Xbox Series X, Microsoft Windows, Xbox Cloud Gaming, OpenXR",
-            genero: "Simulação",
-            idade: require("./assets/classificacao-14-anos.png"),
-            sobre: 'Obrigado aos nossos fãs, novos e de longa data! A Edição de Jogo do Ano (GOTY) do Microsoft Flight Simulator oferece 5 aeronaves inéditas, 8 aeroportos feitos à mão, 6 novos Voos de Descoberta, novos tutoriais e atualizações para o sistema de clima.',
-            requisitos: {
-                so: 'Windows 10',
-                processador: 'Intel i5-8400 | AMD Ryzen 5 1500X',
-                memoria: '16 GB de RAM',
-                placa: ' NVIDIA GTX 970 | AMD Radeon RX 590',
-                armazenamento: '150 GB'
-            }
-        },
-    ]);
+    const [jogos, setJogos] = useState([])
+
+    console.log(jogos);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/jogos')
+            .then((response) => response.json())
+            .then((json) => setJogos(json))
+            .catch((error) => console.error(error))
+    }, []);
+
+
     return (
         <SafeAreaView style={styles.container}>
             <ScrollView>
@@ -124,12 +58,12 @@ function Carrinho({ route, navigation }) {
                 <View>
                     <FlatList
                         showsHorizontalScrollIndicator={false}
-                        data={lancamentos}
+                        data={jogos}
                         renderItem={({ item }) => {
                             return (
                                 <View style={styles.itensCarrinho}>
                                     <View style={{ flex: 1, flexDirection: 'row', }}>
-                                        <Image source={item.image} style={styles.imagemCarrinho} />
+                                        <Image style={styles.imagemCarrinho} />
                                         <View style={{ flex: 1, flexDirection: 'column' }}>
                                             <View style={styles.nomeCarrinho}>
                                                 <Text style={styles.moneyText}>{item.name}</Text>
---
+++ server.js
@@ -0,0 +1,14 @@
+﻿const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const router = require('./routes')
+
+const app = express()
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(cors())
+app.use(router)
+
+app.listen(3000, () => console.log('Servidor iniciado em http://localhost:3000'))
